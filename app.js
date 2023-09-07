bdd4175f0d8339480f8259e4cff6dc9622d1c1c6
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2023-09-07 19:05:03 +0530

    http request 1

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-console.log("Strating");
+// console.log("Strating");
 
-    setTimeout(() => {
-        console.log("2 second timer");
-    },2000);
+//     setTimeout(() => {
+//         console.log("2 second timer");
+//     },2000);
 
-    setTimeout(() => {
-        console.log("0 second timer");
-    },0);
+//     setTimeout(() => {
+//         console.log("0 second timer");
+//     },0);
 
-console.log("Stoping");
+// console.log("Stoping");
+
+
+const request = require("request");
+const url = 'http://api.weatherstack.com/current?access_key=6af5e10c2ff675ee48ab09a7203da892&query=london';
+
+request({url : url} , (error , response) => {
+    // console.log(response);
+
+    const data = JSON.parse(response.body);
+    console.log(data.current)
+})
